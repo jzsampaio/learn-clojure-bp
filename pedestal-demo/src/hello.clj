c1523ab37a4838711f426e8416af6d0357056982
Juarez Sampaio <juarez.filho@brasilparalelo.com.br>
2022-10-11 19:23:28 -0300

    Add pedestal-demo

---
+++ pedestal-demo/src/hello.clj
@@ -0,0 +1,94 @@
+(ns hello
+  (:require [io.pedestal.http :as http]
+            [io.pedestal.http.route :as route]
+            [clojure.data.json :as json]
+            [io.pedestal.http.content-negotiation :as conneg]))
+
+(defn ok [body]
+  {:status 200
+   :body body
+   ; :headers {"Content-Type" "text/html"}
+   })
+
+(defn error [body] {:status 400 :body body})
+
+(defn not-found [] {:status 404 :body "Not found\n"})
+
+(def unmentionables #{"Batini" "Pedro"})
+
+(def supported-types ["text/html" "application/edn" "application/json" "text/plain"])
+
+(def content-neg-intc (conneg/negotiate-content supported-types))
+
+(defn accepted-type
+  [context]
+  (get-in context [:request :accept :field] "text/plain"))
+
+(defn transform-content
+  [body content-type]
+  (case content-type
+    "text/html" body
+    "text/plain" body
+    "application/edn" (pr-str body)
+    "application/json" (json/write-str body)))
+
+(defn coerce-to
+  [response content-type]
+  (-> response
+      (update :body transform-content content-type)
+      (assoc-in [:headers "Content-Type"] content-type)))
+
+(def coerce-body
+  {:name ::coerce-body
+   :leave
+   (fn [context]
+     (cond-> context
+       (nil? (get-in context [:response :headers "Content-Type"]))
+       (update-in [:response] coerce-to (accepted-type context))))})
+
+(defn greeting-for [nm]
+  (cond
+    (= "" nm) nil
+    (unmentionables nm) nil
+    (empty? nm) "Hello, world!\n"
+    :else (str "Hello, " nm "\n")))
+
+(defn respond-hello [request]
+  (let [nm (get-in request [:query-params :name])
+        resp (greeting-for nm)]
+    ( if resp
+      (ok resp)
+     (not-found))))
+
+
+(def echo
+  {:name ::echo
+   :enter #(assoc % :response (ok (:request %)))})
+
+(def routes
+  (route/expand-routes
+   #{["/greet" :get [coerce-body content-neg-intc respond-hello] :route-name :greet]
+     ["/echo" :get echo]}))
+
+(def service-map
+  {::http/routes routes
+   ::http/type :jetty
+   ::http/port 8080})
+
+(defn start []
+  (http/start (http/create-server service-map)))
+
+(defonce server (atom nil))
+
+(defn start-dev []
+  (reset! server
+          (http/start (http/create-server
+                       (assoc service-map
+                              ::http/join? false)))))
+
+(defn stop-dev []
+  (http/stop @server))
+
+(defn restart []
+  (stop-dev)
+  (start-dev))
